6ee971f1ea77560dc68d28f2ad4dd6c12b9787f4
SUKANTA BISWAS <129424328+sukantabiswas27@users.noreply.github.com>
2025-09-22 15:25:29 +0530

    Delete routes directory

--- routes/payment.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { PAY ,Payment_response} from '../controlers/payment.js';
-const PAYMENT_router = express.Router();
-
-PAYMENT_router.post('/payment',PAY);
-PAYMENT_router.post('/payment-response',Payment_response)
-
-
-export default PAYMENT_router;
